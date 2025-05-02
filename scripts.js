0416b21bde31ba5f36805cd24ea924c97af7e824
Joana Dayse <101282171+Joanadayse@users.noreply.github.com>
2025-05-02 16:39:12 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,45 @@
+ const nome = ["arroz", 12, true]
+
+ console.log(nome[2])
+const idade= 23 
+
+// console.log(`ola meu nome é ${nome} e tenho ${idade}`)
+// console.log("meu nome é : " + nome + "" + "tenho " + idade + "anos")
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// const pergunta1 = "Voce gosta de programar ?"
+// const resposta1 = prompt(pergunta1)
+
+// console.log(resposta1)
+
+// const largura= Number(prompt("Digite a largua do retangulo:"))
+// const altura= Number(prompt("Digite a altura do retangulo:"))
+// const area= largura * altura
+
+// console.log(` A area é : ${area}`)
+
+
+// function analizarNumero (numero) {
+//     const ehPar= numero % 2 === 0;
+//     const soma = numero + 10;
+//     const multiplica = numero * numero
+
+//     return `O numero ${numero} é par ? ${ehPar}. Somando com 10 é ${soma} e a o quadrado é ${multiplica} `    
+ 
+// }
+
+// console.log(analizarNumero(4))
+
+// console.log(idade)
+// cumprimentar()
+// cumprimentar("Brenno")
+// cumprimentar("Melyssa")
+
+
+
+
+
+
+
